1e0c89a48ab8fc0011f3a03deb17d3d24e0517c9
iamisam <isamaziz256@gmail.com>
2025-09-30 15:55:20 +0530

    adding option handling to cors

--- app.js
+++ app.js
@@ -65,8 +65,16 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://codeplay-onud.onrender.com",
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
   next();
 });
 
