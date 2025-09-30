37f0596c5636c774cab72e8af848232899550d6c
iamisam <isamaziz256@gmail.com>
2025-09-30 16:16:42 +0530

    update nodemailer config

--- utils/emailService.js
+++ utils/emailService.js
@@ -7,11 +7,14 @@ const generateOtp = () => {
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
+    timeout: 10000,
   });
 
   const mailOptions = {
